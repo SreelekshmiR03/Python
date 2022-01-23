e218f0fecc803bb9c86212f0b0dccabba1001096
SreelekshmiR03 <98249166+SreelekshmiR03@users.noreply.github.com>
2022-01-23 23:13:09 +0530

    Add files via upload

---
+++ Assignment 2.2.py
@@ -0,0 +1,28 @@
+#1
+for i in range(0,25,2):
+    print(i)
+    
+#2
+l=[0]
+for i in l:
+    l.append(i+1)
+    print(i)
+     
+#3
+
+x=('english','maths','computer')
+y=list(x)
+y.append('hindi')
+print(y)
+x=tuple(y)
+print(x)
+
+
+
+
+
+
+
+
+
+
